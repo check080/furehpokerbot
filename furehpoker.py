5d824bba9768aa01f644f48d1371e42ebefe0783
check080 <mwiloveicecream565@gmail.com>
2018-06-10 21:47:52 -0600

    Last few commands
    Added /help, /join, prevented players from using commands midgame, gave admins ability to start new game, added admin commands to the help menu

--- furehpoker.py
+++ furehpoker.py
@@ -1,12 +1,13 @@
 import requests
 import json
 
-class player:
+class player: #send the player a game request IFF currentGame==0 AND online==True
     def __init__(self,plid,username):
         self.id=plid
         self.username=username
         self.chips=1000
         self.currentGame=0
+        self.currentGameInvite=0
         self.online=True
 
 class game:
@@ -18,8 +19,9 @@ class game:
 
 BOT_TOKEN="598223102:AAGRkSBjoZV2KMKLtGAFB7xlMJnN1xShpTA"
 ACCESS_WEBSITE="https://api.telegram.org/bot"+BOT_TOKEN+"/"
-ADMIN_IDS=[156638024,131331518]
-LEADERBOARD_NUM=10
+ADMIN_IDS=[156638024,131331518] #Facade and Alexi
+LEADERBOARD_NUM=10 #How many names to display in the leaderboard
+TIER_CUTOFFS=[0, 1000, 2000, 3000, 4000] #The minimum required chips to be a member of each tier
 
 cont=True
 players={} #plid: playerinstance
@@ -73,6 +75,31 @@ def processCommand(usrReq):
 
     print("FROM %d (%s): %s" %(userid,username,reqtext) ) #THIS will break if someone types arabic
 
+    if(userid in ADMIN_IDS):
+        cmdList=reqtext.split(" ")
+        #ADMIN COMMANDS
+        if(cmdList[0]=="/quit"):
+            print("Admin called quit.")
+            cont=False
+
+        elif(cmdList[0]=="/help"):
+            retString="ADMIN COMMANDS:\n" \
+                      "/quit - Stop the server\n" \
+                      "/startgame [tier] - Start a new game on the specified tier\n" \
+                      "/gamestats - See all game stats"
+            sendMessage(userid,retString)
+            
+        elif(cmdList[0]=="/startgame"):
+            try:
+                tiernum=int(cmdList[1])
+            except ValueError:
+                sendMessage(userid,"Usage: /startgame [tier], where [tier] is the tier the game is on from %d to %d." %(0,len(TIER_CUTOFFS)) )
+                return
+            newGame(tiernum)
+
+        elif(cmdList[0]=="/gamestats"):
+            print("UNIMPLEMENTED")
+
     #USER COMMANDS
     if(reqtext=="/start"):
         if(userid in players.keys() ): #if the player is already registered
@@ -82,11 +109,34 @@ def processCommand(usrReq):
         else: #if they aren't
             players[userid]=player(userid,username) #add a dict entry with a new instance of the player class
             sendMessage(userid,"Welcome to Fur-Eh poker. Explanation text. Type /help for more commands. Your balance is %d chips." %players[userid].chips )
-        sendMessage(userid,"%d players online." %findNumOnline())
+
+    elif(reqtext=="/help"):
+        retString="Bot Commands:\n" \
+                  "/start - Start the bot\n" \
+                  "/help - Get a list of commands\n" \
+                  "/online - Receive invitations to poker games\n" \
+                  "/offline - Stop receiving invitations\n" \
+                  "/getleaderboard - Show the top players and their chips\n" \
+                  "/join - Join the game you've been invited to\n" \
+                  "You cannot use these commands while in the middle of a game."
+        sendMessage(userid,retString)
 
     elif(not userid in players.keys() ): #if the player hasn't yet said /start
         sendMessage(userid,"Start the bot with /start.")
         return #don't let them do any other commands
+
+    elif(players[userid].currentGame!=0): #if the player is in the middle of a game
+        if(reqtext[0]=="/"):
+            sendMessage(userid,"You can't use bot commands in the middle of a game.")
+        else:
+            print("GAME REQUEST RECEIVED.")
+
+    elif(reqtext=="/join"):
+        retString=""
+        if(not players[userid].online):
+            sendMessage(userid,"You are currently marked as offline. Type /online to get game invitations.")
+        elif(players[userid].currentGameInvite==0):
+            sendMessage(userid,"No game to join yet. Wait for an invite, and then type /join")
 
     elif(reqtext=="/offline"):
         if(players[userid].online):
@@ -100,22 +150,17 @@ def processCommand(usrReq):
             sendMessage(userid,"You are already marked as online.")
         else:
             sendMessage(userid,"Ok, you will now receive poker game invitations.")
-        sendMessage(userid,"%d players online." %findNumOnline())
         players[userid].online=True
 
-    elif(reqtext=="/getLeaderboard"):
+    elif(reqtext=="/getleaderboard"):
         leaderList=getLeaderboard()
-        retText=""
+        retText="%d players online.\nTop chips:" %findNumOnline()
         for i in leaderList:
             retText+="\n%s: %s" %(i[0],i[1])
-        sendMessage(userid,"Top chips:"+retText)
-        
-    if(userid in ADMIN_IDS):
-        #ADMIN COMMANDS
-        if(reqtext=="/quit"):
-            print("Admin called quit.")
-            cont=False
+        sendMessage(userid,retText)
     
+def newGame(tiernum):
+    print("Starting new game for tier %d." %tiernum)
 
 def main():
     #Initialization here

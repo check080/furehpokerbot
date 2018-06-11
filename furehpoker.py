bcb60da57083994df8386aa00bb5a3618ff77f46
check080 <mwiloveicecream565@gmail.com>
2018-06-10 20:07:24 -0600

    Added /online, /stop, /offline, /quit, fixed /start

--- furehpoker.py
+++ furehpoker.py
@@ -2,8 +2,9 @@ import requests
 import json
 
 class player:
-    def __init__(self,plid):
+    def __init__(self,plid,username):
         self.id=plid
+        self.username=username
         self.chips=1000
         self.currentGame=0
         self.online=True
@@ -18,6 +19,7 @@ class game:
 BOT_TOKEN="598223102:AAGRkSBjoZV2KMKLtGAFB7xlMJnN1xShpTA"
 ACCESS_WEBSITE="https://api.telegram.org/bot"+BOT_TOKEN+"/"
 ADMIN_IDS=[156638024,131331518]
+LEADERBOARD_NUM=10
 
 cont=True
 players={} #plid: playerinstance
@@ -38,7 +40,18 @@ def botUpdate(num):
         return False
 
 def sendMessage(chat_id,text):
+    print("TO %d: %s" %(chat_id,text) )
     sendBotRequest("sendMessage?chat_id=%d&text=%s" %(chat_id,text) )
+
+def findNumOnline(): #Returns the number of people online
+    totalOnline=0
+    for i in players.values():
+        if(i.online):
+            totalOnline+=1
+    return totalOnline
+
+def getLeaderboard(): #Returns a list of the people with the most chips, greatest to least
+    print("I gave up sorry")
 
 def processCommand(usrReq):
     global cont
@@ -46,23 +59,49 @@ def processCommand(usrReq):
     global games
 
     userid=usrReq["from"]["id"]
+    username=usrReq["from"]["username"]
     reqtext=usrReq["text"]
+
+    print("FROM %d (%s): %s" %(userid,username,reqtext) ) #THIS will break if someone types arabic
 
     #USER COMMANDS
     if(reqtext=="/start"):
         if(userid in players.keys() ): #if the player is already registered
             sendMessage(userid,"Welcome back to Fur-Eh poker. Your balance is %d chips." %players[userid].chips )
+            if(not players[userid].online):
+                sendMessage(userid,"You are currently offline. If you want to get invited to poker games, say /online")
         else: #if they aren't
-            players[userid]=player(userid) #add a dict entry with a new instance of the player class
+            players[userid]=player(userid,username) #add a dict entry with a new instance of the player class
             sendMessage(userid,"Welcome to Fur-Eh poker. Explanation text. Type /help for more commands. Your balance is %d chips." %players[userid].chips )
 
-    #if(userid in ADMIN_IDS):
+    elif(reqtext=="/stop"):
+        players[userid].online=False
+
+    elif(reqtext=="/offline"):
+        if(players[userid].online):
+            sendMessage(userid,"Ok, you will no longer receive poker game invitations.")
+        else:
+            sendMessage(userid,"You are already marked as offline.")
+        players[userid].online=False
+
+    elif(reqtext=="/online"):
+        if(players[userid].online):
+            sendMessage(userid,"You are already marked as online.")
+        else:
+            sendMessage(userid,"Ok, you will now receive poker game invitations.")
+        
+    if(userid in ADMIN_IDS):
         #ADMIN COMMANDS
+        if(reqtext=="/quit"):
+            print("Admin called quit.")
+            cont=False
     
 
 def main():
+    #Initialization here
+    print("Initialized.")
+
     numClear=0
-    
     while cont:
 
         #CHECK FOR PLAYER INPUT
@@ -81,5 +120,8 @@ def main():
                     continue
 
         #CHECK TIMERS
+        
+    botUpdate(numClear) #get rid of the last update queue
+
 
 main()
